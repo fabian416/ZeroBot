cdf86090d665403b7a63b65731b46b1be5297f40
slujan <slujan@ratherlabs.com>
2025-06-21 05:00:17 +0200

    feat: update repository architecture

--- discord-bot/server/server.js
+++
@@ -1,9 +0,0 @@
-import routes from './routes/routes.js';
-
-Bun.serve({
-  port: Bun.env.PORT,
-  ...routes, 
-  fetch(req) {
-    return new Response("404, ZK Not found", { status: 404 }); 
-  },
-});
