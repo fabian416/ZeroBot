f476758026b71c1452930f4619df648919f2bbf6
fabiandiaz416 <diazfabian99@icloud.com>
2025-06-22 14:11:25 +0200

    feat: improving circuit and fixing backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,15 +1,13 @@
 import { StatusController } from '../controllers/StatusController.js';
 import { StatusService } from '../services/StatusService.js';
 import { StatusRepository } from '../repositories/StatusRepository.js';
-import NoirController from '../controllers/NoirService.js';
+// import NoirController from '../controllers/NoirService.js';
 
 // Dependency injection setup
 const statusRepository = new StatusRepository();
 const statusService = new StatusService(statusRepository);
 const statusController = new StatusController(statusService);
 
-
-const noirController = new NoirController();
 
 export default {
     routes: {
@@ -18,8 +16,10 @@ export default {
             GET: async (req, res) => { return statusController.get(req, res); },
         },
     },
+    /**
     '/api/noir': {
         POST: async (req, res) => { return noirController.deployZeroBot(req, res); },
         GET: async (req, res) => { return noirController.deployIdentity(req, res); },
     },
+    */
 }
